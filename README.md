c8331154a9bdad0227e91bbaf6659ef3fa450e7a
nature-prakriti <nature.prakriti123@gmail.com>
2023-03-10 17:30:25 +0530

    Prakriti: login done, student-registration done

---
+++ README.md
@@ -0,0 +1,16 @@
+Clone this project
+
+pip3 install -r requirements.txt
+
+Change the password in db.yaml to that of your MySQL's password
+
+Run the application by executing the command python3 app.py
+
+The application runs on localhost:5000
+
+
+mysql_host: 'localhost'
+mysql_user: 'root'
+# Enter your password in field below
+mysql_password: '**********'
+mysql_db: 'placement_management_system'
---
+++ app1.py
@@ -0,0 +1,128 @@
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+import yaml
+import json
+
+
+app = Flask(__name__)
+
+# Configure db
+db = yaml.safe_load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        # Fetch form data
+        userDetails = request.form
+        user_id = userDetails['user_id']
+        password = userDetails['password']
+
+        cur = mysql.connection.cursor()
+        
+        cur.execute("SELECT * FROM person WHERE person_id=%s AND password_hash=%s",(user_id, password))
+
+        user = cur.fetchone()
+
+        if user:
+            # successful login, redirect to home page
+            return redirect('/users')
+        else:
+            # invalid login, show error message
+            error = 'Invalid username or password'
+            return render_template('login/login.html', error=error)
+        
+    else:
+        return render_template('login/login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # Fetch form data
+        userDetails = request.form
+        role = userDetails['role']
+        if (role=="student"):
+            return redirect('/student-registration')
+        elif (role=="company_rep"):
+            return render_template('login/company_rep.html')
+        elif (role=="admin"):
+            return render_template('login/admin.html')
+    else:
+        return render_template('login/register.html')
+
+@app.route('/student-registration', methods=['GET', 'POST'])
+def student_reg():
+    if request.method == 'POST':
+        userDetails = request.form
+        person_id = userDetails['person_id'],
+        first_name = userDetails['first_name'],
+        middle_name = userDetails['middle_name'],
+        last_name = userDetails['last_name'],
+        country_code = userDetails['country_code'],
+        mobile_number = userDetails['mobile_number'],
+        email_id = userDetails['email_id'],
+        profile_photo = userDetails['profile_photo'],
+        password = userDetails['password'],
+        nationality = userDetails['nationality'],
+        cpi = userDetails['cpi'],
+        backlogs = userDetails['backlogs'],
+        dob = userDetails['dob'],
+        category = userDetails['category'],
+        gender = userDetails['gender'],
+        experience = userDetails['experience'],
+        personal_email = userDetails['personal_email'],
+        curr_program = userDetails['curr_program'],
+        joining_date = userDetails['joining_date'],
+        year_of_graduation = userDetails['year_of_graduation'],
+        resume = userDetails['resume'],
+        major_disc = userDetails['major_disc'],
+        minor_disc = userDetails['minor_disc']
+        x = {"country_code":country_code, "number":mobile_number}
+        cur = mysql.connection.cursor()
+
+        cur.execute("INSERT INTO person(person_id, first_name, middle_name, last_name, mobile_number, email, profile_photo, password_hash, nationality, person_role) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                    (person_id,first_name, middle_name, last_name, json.dumps(x), email_id, profile_photo, password, nationality, "Student"))
+        
+        print("added to person table")
+        
+        cur.execute("INSERT INTO student (person_id, cpi, backlogs, category, gender, dob, professional_experience, personal_mail, year_of_graduation, current_program, cv, major_disciplines, minor_disciplines, date_of_joining,parent_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                    (person_id,cpi, backlogs, category, gender, dob, experience, personal_email, userDetails['year_of_graduation'][0:4], curr_program, resume, major_disc, minor_disc, joining_date, person_id))
+        
+        mysql.connection.commit()
+
+        # if inserted in student, will be in person for sure bcz of foreign key contraint
+        cur.execute("SELECT * FROM student WHERE person_id=%s",(person_id))
+
+        user = cur.fetchone()
+        print("user")
+        print(user)
+        cur.close()
+        if user:
+            # successful login, redirect to home page
+            return redirect('/users')
+        else:
+            # invalid login, show error message
+            error = 'Invalid username or password'
+            return render_template('login/student.html', error=error)
+        
+        
+        print("ALL DONE")
+        return render_template('login/student.html')
+    else:
+        return render_template('login/student.html')
+    
+@app.route('/users')
+def users():
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM users")
+    if resultValue > 0:
+        userDetails = cur.fetchall()
+        return render_template('users.html',userDetails=userDetails)
+
+if __name__ == '__main__':
+    app.run(debug=True)
